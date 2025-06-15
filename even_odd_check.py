5caf6ffce32b5860118e38e91888172dd9a90b3b
Vecha Pavan Kumar <wisdomshare1969@gmail.com>
2025-06-15 12:52:41 +0530

    Implemented even and odd functionality check

--- even_odd_check.py
+++ even_odd_check.py
@@ -1,1 +1,12 @@
-print("Even")
+while True:
+    try:
+        num = int(input("enter the integer number = "))
+        break
+    except:
+        print("Not an integer number")
+remainder = num % 2
+if remainder == 0 :
+    print("even")
+else:
+    print("odd")
+    
